445572a96e4d45718536f1c52597190612a8ed26
Daniel James dolor <danieljames.dolor@amadeus.com>
2024-02-15 15:57:32 +0800

    asdasfqafqw

--- src/Something.Api/Controllers/AccountController.cs
+++ src/Something.Api/Controllers/AccountController.cs
@@ -1,11 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Something.Core;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace Something.Api.Controllers;
 
-[Route("api/[controller]")]
+[Route("[controller]")]
 [ApiController]
 [RequireHttps]
 public class AccountController : ControllerBase
