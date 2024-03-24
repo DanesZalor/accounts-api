225d546075a66af5982f8be4fedd60b2ab1f6649
Daniel James dolor <danieljames.dolor@amadeus.com>
2024-03-24 18:17:38 +0800

    fixing stuff

--- scripts/Base64Decode.ps1
+++
@@ -1,6 +0,0 @@
-param(
-    [Parameter(Mandatory=$True, ValueFromPipeline = $true)]
-    [string]$value
-)
-
-return [System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String($value))
